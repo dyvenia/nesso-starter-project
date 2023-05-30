f9a07a986071403674c141d7d0c2824e1425011a
AnnaGerlich <gerlichanna@gmail.com>
2023-05-30 11:41:43 +0200

    ✨ Added `prefect_deployments.py` script

---
+++ .github/scripts/prefect_deployments.py
@@ -0,0 +1,156 @@
+import asyncio
+import os
+import subprocess
+
+from packaging import version
+from prefect.client import OrionClient
+from prefect.settings import PREFECT_API_KEY, PREFECT_API_URL
+
+from prefect import __version__
+
+if version.parse(__version__) >= version.parse("2.10"):
+    from prefect.client.orchestration import get_client
+
+    async def get_prefect_deployments():
+        """Retrieve a list of Prefect deployments."""
+        async with get_client() as client:
+            deployments = await client.read_deployments()
+
+            return deployments
+
+else:
+
+    async def get_prefect_deployments():
+        """Retrieve a list of Prefect deployments."""
+        async with OrionClient(
+            api=PREFECT_API_URL.value(), api_key=PREFECT_API_KEY.value()
+        ) as client:
+            deployments = await client.read_deployments()
+
+            return deployments
+
+
+def create_deployment(file_name: str):
+    """
+    Create a deployment from the specified file_name.
+    Args:
+        file_name (str): The file_name to the deployment script.
+    Raises:
+        subprocess.CalledProcessError: If the deployment creation fails.
+    """
+    try:
+        print(f"Creating deployment {file_name} ...")
+        subprocess.run(["python3", file_name], check=True)
+        print(f"Successfully completed {file_name} deployment creation")
+    except subprocess.CalledProcessError as e:
+        print(f"FAILED to create deployment {file_name}")
+        raise e
+
+
+def delete_deployment(deployment_id: str, deployment_name: str):
+    """
+    Delete a deployment with the specified id.
+    Args:
+        deployment_name (str): The name of the deployment.
+    Raises:
+        subprocess.CalledProcessError: If the deployment deletion fails.
+    """
+    try:
+        print(f"Deleting deployment {deployment_name} ...")
+        delate_command = ["prefect", "deployment", "delete", "--id", deployment_id]
+        subprocess.run(delate_command, capture_output=True, text=True)
+        print(f"Successfully completed {deployment_name} deployment deleting")
+    except subprocess.CalledProcessError as e:
+        print(f"FAILED to delete deployment {deployment_name}")
+        raise e
+
+
+def get_current_branch():
+    current_branch_command = ["git", "branch", "--show-current"]
+    current_branch_result = subprocess.run(
+        current_branch_command, capture_output=True, text=True
+    )
+    current_branch = current_branch_result.stdout.strip()
+
+    return current_branch
+
+
+def get_commits(main_branch, current_branch):
+    # Get the commit hashes for the given branch
+    commit_hashes_command = [
+        "git",
+        "log",
+        "--no-merges",
+        current_branch,
+        f"^{main_branch}",
+        "--format=%H",
+    ]
+    commit_hashes_result = subprocess.run(
+        commit_hashes_command, capture_output=True, text=True
+    )
+    commit_hashes = commit_hashes_result.stdout.strip().split()
+    return commit_hashes
+
+
+def get_modified_files(commit_hash):
+    changed_files_command = [
+        "git",
+        "diff-tree",
+        "--no-commit-id",
+        "--name-status",
+        "-r",
+        commit_hash,
+    ]
+    changed_files_result = subprocess.run(
+        changed_files_command, capture_output=True, text=True
+    )
+
+    list_of_change = changed_files_result.stdout.strip().split()
+    dict_of_change = {
+        list_of_change[i + 1]: list_of_change[i]
+        for i in range(0, len(list_of_change), 2)
+    }
+    return dict_of_change
+
+
+def run_deployment(path, operation, prefect_deployments, script_directory):
+    os.chdir(script_directory)
+    file_name = os.path.basename(path)
+
+    if path.startswith("prefect/flows/deployments/") and operation in [
+        "A",
+        "M",
+        "R",
+        "C",
+    ]:
+        print(f"Creating deployment from {path}")
+        os.chdir("../../prefect/flows/deployments/")
+        if os.path.exists(file_name):
+            create_deployment(file_name)
+
+    elif path.startswith("prefect/flows/deployments/") and operation == "D":
+        print(f"Deleting deployment from {path}")
+        for deployment in prefect_deployments:
+            if file_name in deployment.tags:
+                delete_deployment(str(deployment.id), deployment.name)
+
+
+current_branch = get_current_branch()
+
+commit_hashes = get_commits(main_branch="master", current_branch=current_branch)
+
+loop = asyncio.get_event_loop()
+prefect_deployments = loop.run_until_complete(get_prefect_deployments())
+loop.close()
+script_directory = os.getcwd()
+
+implemented_deployments = []
+
+# Iterate over commit hashes
+for commit_hash in commit_hashes:
+    dict_of_change = get_modified_files(commit_hash)
+
+    for path, operation in dict_of_change.items():
+        if path not in implemented_deployments:
+            run_deployment(path, operation, prefect_deployments, script_directory)
+            implemented_deployments.append(path)
